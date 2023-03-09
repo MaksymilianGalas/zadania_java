65133eb1e0104b4a21ff7006dfcb7342c6e0ba2c
MaksymilianGalas <122981146+MaksymilianGalas@users.noreply.github.com>
2023-03-09 21:35:27 +0100

    Update zad2_3.java

--- lab02/zad2_3.java
+++ lab02/zad2_3.java
@@ -8,19 +8,23 @@ public class zad2_3 {
         int[] tablica = new int[n];
         int[] tablica1 = new int[n];
         int[] wynik = new int[n];
-        System.out.println("Podaj wartosi pierwszej tabeli: ");
-        for(int i=0; i<n; i++)
-        {
-            tablica[i]=scan.nextInt();
+        wpisywanie(tablica, tablica1, n, wynik);
+
+    }
+
+    public static int wpisywanie(int[] tablica, int[] tablica1, int n, int[] wynik) {
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            tablica[i] = scan.nextInt();
         }
         System.out.println("Podaj wartosi drugiej tabeli: ");
-        for(int i=0; i<n; i++)
-        {
-            tablica1[i]=scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            tablica1[i] = scan.nextInt();
         }
-        for(int i =0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             wynik[i] = tablica[i] + tablica1[i];
         }
         System.out.println(Arrays.toString(wynik));
+        return 0;
     }
 }
