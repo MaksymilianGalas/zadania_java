537ffdc153047942551829c9629aca58f1dd639b
MaksymilianGalas <122981146+MaksymilianGalas@users.noreply.github.com>
2023-06-11 16:27:32 +0200

    Add files via upload

--- Project/Main.java
+++ Project/Main.java
@@ -26,46 +26,60 @@ public class Main extends RandomWords{
         String wrongLetters = "";
         while (true) {
             System.out.println("Input one letter: ");
-            playerGuess = scan.nextLine();
-            if (playerGuess.length() != 1 || !playerGuess.matches("[a-zA-Z]")) {
-                System.out.println("Wrong input. Please enter a single letter.");
-            } else {
-                if (drawn.contains(playerGuess)) {
-                    for (int i = 0; i < drawn.length(); i++) {
-                        if (playerGuess.equals(wordArr[i])) {
-                            guessedArr[i] = playerGuess;
+            try {
+                playerGuess = scan.nextLine();
+                try {
+                    if (playerGuess.length() != 1) {
+                        throw new IllegalArgumentException("Too much data, try again");
+                    } else if (!playerGuess.matches("[a-ząćęłńóśźż]")) {
+                        if (playerGuess.matches("[A-Z]")) {
+                            throw new IllegalArgumentException("Please input small letters only");
+                        } else {
+                            throw new IllegalArgumentException("Please input letters only");
+                        }
+                    } else {
+                        if (drawn.contains(playerGuess)) {
+                            for (int i = 0; i < drawn.length(); i++) {
+                                if (playerGuess.equals(wordArr[i])) {
+                                    guessedArr[i] = playerGuess;
+                                }
+                            }
+                        } else {
+                            if (!wrongLetters.contains(playerGuess)) {
+                                wrongLetters += playerGuess + ",";
+                            } else {
+                                System.out.println("Don't input the same letter twice.");
+                                continue;
+                            }
+                        }
+
+                    }
+
+
+                    if (!wrongLetters.isEmpty()) {
+                        if ((wrongLetters.length() - 1) < 13) {
+                            gallows.gallows((wrongLetters.length() - 1));
+                            System.out.println("Wrong letters: " + wrongLetters.substring(0, wrongLetters.length() - 1));
+                        } else {
+                            gallows.gallows((wrongLetters.length() - 1));
+                            break;
                         }
                     }
-                } else {
-                    if (!wrongLetters.contains(playerGuess)) {
-                        wrongLetters += playerGuess + ",";
-                    } else {
-                        System.out.println("Don't input the same letter twice.");
-                        continue;
+                    if (Arrays.equals(wordArr, guessedArr)) {
+                        System.out.println("\nCongratulations! You guessed the word correctly.");
+                        break;
                     }
+
+                    for (int j = 0; j < drawn.length(); j++) {
+                        System.out.printf(guessedArr[j]);
+                    }
+                    System.out.println("\n");
+                } catch (NullPointerException e) {
+                    System.out.println("Please input one letter");
                 }
-
+            }catch (IllegalArgumentException ex){
+                System.out.println(ex.getMessage());
             }
-
-
-            if (!wrongLetters.isEmpty()) {
-                if((wrongLetters.length()-1)<13){
-                gallows.gallows((wrongLetters.length()-1));
-                System.out.println("Wrong letters: " + wrongLetters.substring(0, wrongLetters.length() - 1));
-                }else{
-                    gallows.gallows((wrongLetters.length()-1));
-                    break;
-                }
-            }
-            if (Arrays.equals(wordArr, guessedArr)) {
-                System.out.println("\nCongratulations! You guessed the word correctly.");
-                break;
-            }
-
-            for (int j = 0; j < drawn.length(); j++) {
-                System.out.printf(guessedArr[j]);
-            }
-            System.out.println("\n");
         }
 
     }
